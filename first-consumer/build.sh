531950598d5e01da860b48b841daa7928930bf22
ChenWei <545764502@qq.com>
2020-07-30 15:36:23 +0800

    修改pom

---
+++ first-consumer/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+version=$1
+
+if [ $version = dev ] || [ $version = test ] || [ $version = pre ] || [ $version = prod ]
+then
+    mvn clean package -Dmaven.test.skip=true -U
+
+    rm ./docker/*.jar
+
+    mv ./target/first-consumer-1.0-SNAPSHOT.jar ./docker
+
+    echo '开始构建镜像。。。。'
+    docker build -t first-consumer ./docker
+    echo '完成构建镜像。。。。'
+
+    echo '开始启动镜像。。。。'
+    docker run -e "JAVA_OPTS=--spring.profiles.active=${version}" -d -p 3323:3323 first-consumer
+else
+	echo '输入参数不正确！'
+fi
