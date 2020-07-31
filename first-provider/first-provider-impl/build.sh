3c30c597c9812e7785c33a1212bca3987724f2f9
zhangky <1226618137@qq.com>
2020-07-31 15:40:55 +0800

    dockerfile 07-31

--- first-provider/first-provider-impl/build.sh
+++ first-provider/first-provider-impl/build.sh
@@ -12,13 +12,13 @@ rm ./docker/*.jar
 mv ./target/first-provider-impl-1.0-SNAPSHOT.jar ./docker
 
 echo '开始构建镜像。。。。'
-docker build -t first-provider ./docker
+docker build -t first-provider-impl ./docker
 # -t 给镜像起个名字（eureka-test）
 # ./docker 使用的Dockerfile文件的位置
 echo '完成构建镜像。。。。'
 
 echo '开始启动镜像。。。。'
-docker run -e "spring.profiles.active=${version}" -d -p 3322:3322 first-provider
+docker run -e "JAVA_OPTS=--spring.profiles.active=${version}" -d -p 3322:3322 first-provider-impl
 # docker run -d -p 13321:3321 eureka-test
 # -d参数是让容器后台运行
 # -p 是做端口映射，此时将服务器中的13321端口映射到容器中的3321(项目中端口配置的是3321)端口
